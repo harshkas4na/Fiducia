0eb697b265f984300b50ca34b25b82210e7fcdf7
PRakhar <srivastavaprakhar3010@gmail.com>
2024-07-20 15:55:05 +0530

    finalizing the design

--- apps/web/app/Home/shareholdings/page.tsx
+++ apps/web/app/Home/shareholdings/page.tsx
@@ -7,6 +7,10 @@ import {
   FaWallet,
   FaChartPie,
   FaMoneyBillWave,
+  FaShare,
+  FaBullhorn,
+  FaMoneyBillWaveAlt,
+  FaPiedPiper,
 } from "react-icons/fa";
 
 export default function ShareholdingsPage() {
@@ -124,25 +128,25 @@ export default function ShareholdingsPage() {
           <div className="absolute inset-0 shadow-inner shadow-blue-500/50"></div>
           <div className="relative z-10">
             <h2 className="text-2xl font-semibold mb-6 text-blue-300 text-center border border-blue-300 rounded-lg py-2">
-              Withdraw Funds
+              Withdraw Shareholdings
             </h2>
             <div className="text-center mb-6">
-              <p className="text-sm text-blue-200 mb-2">MemeCoin Balance</p>
+              <p className="text-sm text-blue-200 mb-2">Your Share</p>
               <p className="text-2xl font-bold text-white">
-                <FaCoins className="inline mr-2 text-yellow-400" />
-                {memeCoins} MEME
+                <FaChartPie className="inline mr-2 text-green-400" />
+                10%
               </p>
             </div>
             <div className="flex justify-center">
               <button
                 onClick={handleWithdraw}
-                className="w-40 bg-transparent border-2 border-green-400 text-green-400 font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 text-sm relative overflow-hidden group"
+                className="w-40 bg-transparent border-2 border-red-400 font-bold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 text-sm relative overflow-hidden group"
               >
                 <span className="relative z-10 text-slate-50">
-                  Withdraw Funds
+                  Withdraw Share
                 </span>
-                <div className="absolute inset-0 bg-green-400 opacity-20 group-hover:opacity-30 transition-opacity duration-300"></div>
-                <div className="absolute inset-0 shadow-[inset_0_0_10px_rgba(74,222,128,0.5)] group-hover:shadow-[inset_0_0_15px_rgba(74,222,128,0.7)] transition-shadow duration-300"></div>
+                <div className="absolute inset-0 bg-red-400 opacity-20 group-hover:opacity-30 transition-opacity duration-300"></div>
+                <div className="absolute inset-0 shadow-[inset_0_0_10px_rgba(255,49,49,0.5)] group-hover:shadow-[inset_0_0_15px_rgba(255,50,128,0.7)] transition-shadow duration-300"></div>
               </button>
             </div>
           </div>
